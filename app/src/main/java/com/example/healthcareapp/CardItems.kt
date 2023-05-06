5c352a156a7abe9c4516eb2f9b3c6c7b4189bb74
IT21334542 <it21334542@my.sliit.lk>
2023-05-06 15:09:58 +0530

    Revert "Completed Pharmacist Features"
    This reverts commit f27554b27a0679a097730ae5959cd233a5d01b90.

--- app/src/main/java/com/example/healthcareapp/CardItems.kt
+++
@@ -1,5 +0,0 @@
-package com.example.healthcareapp
-
-class CardItems {
-    //items
-}
--- app/src/main/java/com/example/healthcareapp/Pharmarcist_QRscanner.kt
+++ app/src/main/java/com/example/healthcareapp/Pharmarcist_QRscanner.kt
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 
-class DashboardPharmacist : AppCompatActivity() {
+class Pharmarcist_QRscanner : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pharmarcist_qrscanner)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(R.layout.activity_dashboard_pharmacist)
 
 
     }
